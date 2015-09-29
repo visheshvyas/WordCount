a3acbb8263cb7cc19db3b3e2eb86c11336338de1
visheshvyas <vyas_vishesh@yahoo.com>
2015-09-29 20:10:39 +0100

    adding wordcount folder

---
+++ WordCount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is the default string
+            string _authorString = "This is a statement, and so is this.";
+
+            while (!string.IsNullOrEmpty(_authorString))
+            {
+                foreach (var data in Sentence.CheckCount(_authorString))
+                {
+                    //showing the o/p in console
+                    Console.WriteLine(data.word + "-->" + data.count);  
+                }
+                
+                Console.WriteLine("Press enter to quit... OR provide sentence as an input...");
+
+                //user input will be set as a next string
+                //as long as user has provided sentence, loop will continue
+                //and show the output
+                _authorString = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WordCount/Sentence.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WordCount
+{
+    //struct to hold the data for a word
+    public struct structWords
+    {
+        public string word;
+        public int count;
+    }
+
+    //sentence class 
+    //this class will have all the methods to work on sentence
+    public class Sentence
+    {
+        //characters that cannot be removed from a sentence to differentiate the word
+        const string regExp= @"[^0-9a-zA-Z'\s]+";
+
+        //assigning into list so that test can be done for individual words
+        static List<structWords> lstData = new List<structWords>();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="authorString"></param>
+        /// <returns></returns>
+        public static List<structWords> CheckCount(string authorString)
+        {
+            //replacing all the punctuation marks
+            authorString = Regex.Replace(authorString, regExp, "");
+
+            //splitting sentence into words 
+            var arrWords = authorString.ToLower().Split(new char[] { ' ' });
+
+            //grouping the words and getting the count
+            var wordCnt = from col in arrWords
+                          group col by col into g
+                          select new structWords { word = g.Key, count = g.Count() };
+
+            lstData.AddRange(wordCnt);
+            
+            return lstData;
+        }
+    }
+}
